c770e2dd1443238a37117136108da216dffd8e14
Ian Liu <ian1654@gmail.com>
2017-06-19 20:00:41 +0800

    Add Pin layout!

--- pinmersive/static/js/base.js
+++ pinmersive/static/js/base.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-    $('.dropdown-toggle').dropdown()
+    $('.dropdown-toggle').dropdown();
+
+    $('.grid').isotope({
+        itemSelector: '.grid-item',
+        masonry: {
+            columnWidth: 20,
+            gutter: 0,
+            fitWidth: true
+        }
+    });
+
+    var elements = $('.grid').find('.grid-item');
+    $('.grid').addClass('is-showing-items').isotope('revealItemElements', elements);
 });
