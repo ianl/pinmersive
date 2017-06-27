922c0d2df8e134753156b08adb97e210300472f1
Ian Liu <ian1654@gmail.com>
2017-06-27 17:44:25 +0800

    Fix pins/show.js

--- pinmersive/static/js/pins/show.js
+++ pinmersive/static/js/pins/show.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
     
     $(document).mouseup(function(e) {
-        var container = $('.pin-container > .modal-dialog .modal-content, #pin-show-body> .modal-forms');
+        var container = $('.pin-container > .modal-dialog .modal-content, .modal-forms');
         if (!container.is(e.target) && container.has(e.target).length === 0) {
             parent.history.back();
             return false;
         }
     });
+    
 });
