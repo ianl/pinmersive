dfd935729cc02620eb24cf9f9c5d0a95af518f6b
Ian Liu <ianthl@outlook.com>
2018-07-29 02:14:55 +0800

    Migrate from SQLite3 to PostgreSQL

--- pinmersive/settings.py
+++ pinmersive/settings.py
@@ -98,11 +98,22 @@ WSGI_APPLICATION = 'pinmersive.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pinmersive',
+        'USER': 'Ian',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
